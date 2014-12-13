9b25e74ca209c7c87ac0fac4609bc572fd720a7c
quinston <lklq@live.com>
2014-12-12 22:27:40 -0500

    Fetches course description page from URL over HTTP

---
+++ fetchCourseAvailability.py
@@ -0,0 +1,13 @@
+import urllib.request
+from html.parser import HTMLParser
+
+def fetchHtmlInSubject(subject):
+	""" Downloads the page of the undergraduate calendar for all courses of the given subject. """
+	url = "http://ugradcalendar.uwaterloo.ca/courses/{}".format(subject)
+	response = urllib.request.urlopen(url)
+	data = response.read()
+	return data
+
+def getCourseTableTags(html):
+	"""Returns a list of strings, each string which is a <table></table> tag containing all the information on one course."""
+	
