89b5cb65f3caf27a33a4f0bbba6334e3564ab495
quinston <lklq@live.com>
2014-12-24 20:10:08 -0500

    Get stuff to print heights

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,35 @@
 #include<string>
 #include<list>
 #include<iostream>
+#include<vector>
 
 enum 
 {
 CS135,
 CS145,
+CS135orCS145,
 CS146,
 CS136,
 n_courses
 };
 
-const char* noms[] = {"135","145","146","136"};
+const char* noms[] = {"135","145","135 or 145","146","136"};
 
 typedef std::pair<int, int> Edge;
 
 
 Edge prereqTo[] = 
-{{CS135, CS136},
-{CS145, CS136},
+{
+{CS135, CS135orCS145},
+{CS145, CS135orCS145},
+{CS135orCS145, CS136},
 {CS145, CS146}};
 
 
 
 typedef boost::adjacency_list<boost::vecS, 
 boost::vecS, 
-boost::directedS> Graph;
+boost::bidirectionalS> Graph;
 typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
 
 Graph g(prereqTo, prereqTo + sizeof(prereqTo) / sizeof(Edge), n_courses);
@@ -40,5 +44,21 @@ for (Vertex v : topo_order) {
  std::cout << noms[v] << std::endl;
 }
 std::cout << std::endl;
+
+std::vector<int> height(n_courses, 0);
+
+for (Vertex v : topo_order) {
+ if (in_degree(v, g) != 0) {
+  Graph::in_edge_iterator begin, end;
+  std::tie(begin, end) = boost::in_edges(v, g);
+  auto highestVertex = boost::source(*std::max_element(begin, end, [&height](const boost::graph_traits<Graph>::edge_descriptor& a, const boost::graph_traits<Graph>::edge_descriptor& b) { return height[boost::source(a,g)] < height[boost::source(b,g)]; }), g);
+  height[v] = height[highestVertex] + 1;
+ }
+}
+
+for (int i = 0; i != n_courses; ++i) {
+ std::cout << noms[i] << " can be taken no earlier than term " << (height[i] + 1) << std::endl;
+}
+
 return 0;
 }
