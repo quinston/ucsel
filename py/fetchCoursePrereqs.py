cbaec0eed32754999d2b322af80034aa574037fe
quinston <lklq@live.com>
2015-02-01 14:36:37 -0500

    Add automatic API query for course prereqs

---
+++ py/fetchCoursePrereqs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import urllib.request
+import getCourseList
+
+
+apiUrl = "https://api.uwaterloo.ca/v2/courses/{subject}/{catalogueNumber}/prerequisites.json?key={key}"
+dataDirectory = "../data"
+
+"""
+key: Waterloo OpenData API key
+"""
+def fetchCoursePrereqs(key):
+ courses = getCourseList.getCourseList()
+ 
+ for subject, catalogueNumber in courses:
+  f = open("{path}/{subject}-{catalogueNumber}.json".format(path=dataDirectory, subject=subject, catalogueNumber=catalogueNumber), "w")
+  r = urllib.request.urlopen(apiUrl.format(subject=subject, catalogueNumber=catalogueNumber, key=key))
+  f.write(r.read().decode("utf8"))
+  f.close()
