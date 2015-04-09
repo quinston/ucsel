14cea51916a881fb56efc6e5e7479d905149b74c
quinston <lklq@live.com>
2015-04-08 20:40:00 -0400

    GUILE interpreter works
    Got GUILE CMake modules from Opencog

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
+#include<vector>
+#include<libguile.h>
+#include "base/logging.h"
+#include "constraint_solver/constraint_solver.h"
 
-int main() {
+using operations_research::Solver;
+using operations_research::DecisionBuilder;
+using operations_research::IntVar;
+using operations_research::Constraint;
+using std::cout;
+
+static void inner_main(void *data, int argc, char **argv) {
+ scm_shell (argc, argv);
+}
+
+int main(int argc, char** argv) {
+ scm_boot_guile(argc, argv, inner_main, 0);
+
  return 0;
 }
