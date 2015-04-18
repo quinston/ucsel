ecde1a74fe295f130a7875258b5fc7ddad5ec75b
quinston <lklq@live.com>
2015-04-17 20:32:57 -0400

    Put dataDirectory in a common module

--- py/fetchCourseInfo.py
+++ py/fetchCourseInfo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import urllib.request
 import getCourseList
-
+import common
 
 apiUrl = "https://api.uwaterloo.ca/v2/courses/{subject}/{catalogueNumber}.json?key={key}"
-dataDirectory = "../data/info"
+destDirectory = common.dataDirectory + "/info"
 
 """
 key: Waterloo OpenData API key
@@ -13,7 +13,7 @@ def fetchCourseInfo(key):
  courses = getCourseList.getCourseList()
  
  for subject, catalogueNumber in courses:
-  f = open("{path}/{subject}-{catalogueNumber}.json".format(path=dataDirectory, subject=subject, catalogueNumber=catalogueNumber), "w")
+  f = open("{path}/{subject}-{catalogueNumber}.json".format(path=destDirectory, subject=subject, catalogueNumber=catalogueNumber), "w")
   r = urllib.request.urlopen(apiUrl.format(subject=subject, catalogueNumber=catalogueNumber, key=key))
   f.write(r.read().decode("utf8"))
   f.close()
